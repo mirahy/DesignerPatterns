743ac18520b48b5ad81301f4ab728cd9cab98329
Mirahy <mirahy@gmail.com>
2022-04-11 21:59:22 -0400

    Projeto core

---
+++ Core/src/main/java/br/edu/ifms/core/Desconto.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package br.edu.ifms.core;
+
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author aluno
+ */
+public interface Desconto {
+    BigDecimal calcular(Pedido pedido);
+    void setProximo(Desconto proximo);
+    
+}
