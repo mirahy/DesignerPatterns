4c5fb39a9a5e88912216bbb5b21042946efcf4a7
Mirahy <mirahy@gmail.com>
2022-06-06 22:49:42 -0400

    Agenda

---
+++ Agenda/src/main/java/br/edu/ifms/agenda/dao/ContatoDao.java
@@ -0,0 +1,104 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifms.agenda.dao;
+
+import br.edu.ifms.agenda.database.Conexao;
+import br.edu.ifms.agenda.model.Contato;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import org.hibernate.HibernateException;
+
+/**
+ *
+ * @author aluno
+ */
+public class ContatoDao {
+    
+     private static final String JPQL = "SELECT c FROM Contato c";
+
+    private EntityManager getEntityManager() {
+        return Conexao.createEntityManager();
+    }
+
+    
+    public List<Contato> buscarPorNome(String nome) {
+        EntityManager em = getEntityManager();
+        String condicao = "";
+        List<Contato> clientes = null;
+        Boolean hasNome = nome != null && !nome.isBlank() && !nome.isEmpty();
+        if (hasNome) {
+            condicao = " WHERE c.nome LIKE ?1 ";
+        }
+        Query query = em.createQuery(JPQL + condicao);
+        if (hasNome) {
+            clientes = query.setParameter(1, "%" + nome + "%")
+                    .getResultList();
+        } else {
+            clientes = query.getResultList();
+        }
+        em.close();
+        return clientes;
+    }
+
+
+    public Contato inserir(Contato object) {
+        EntityManager em = getEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.persist(object);
+            em.getTransaction().commit();
+        } catch (HibernateException ex) {
+            em.getTransaction().rollback();
+        }
+        em.close();
+        return object;
+    }
+
+ 
+    public Contato alterar(Contato object) {
+        EntityManager em = getEntityManager();
+        em.detach(object);
+        try {
+            em.getTransaction().begin();
+            em.merge(object);
+            em.getTransaction().commit();
+        } catch (HibernateException ex) {
+            em.getTransaction().rollback();
+        }
+        em.close();
+        return object;
+    }
+
+   
+    public void excluir(Object object) {
+        Long id = (Long) object;
+        EntityManager em = getEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.remove(em.getReference(Contato.class, id));
+            em.getTransaction().commit();
+        } catch (HibernateException ex) {
+            em.getTransaction().rollback();
+        }
+        em.close();
+    }
+
+
+    public List<Contato> listar() {
+        return buscarPorNome(null);
+    }
+
+
+    public Contato buscarPorId(Object object) {
+        EntityManager em = getEntityManager();
+        Long id = (Long) object;
+        Contato obj = em.find(Contato.class, id);
+        em.close();
+        return obj;
+    }
+
+    
+}
---
+++ Agenda/src/main/java/br/edu/ifms/agenda/database/Conexao.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifms.agenda.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author santos
+ */
+public class Conexao {
+    
+    private Connection conn;
+    private static Conexao conexao;
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("Agenda");
+    
+    private Conexao() throws SQLException {
+        String url = "jdbc:postgresql://10.10.34.20/agenda?user=postgres&password=postgres";
+        this.conn = DriverManager.getConnection(url);
+    }
+    
+    public static EntityManager createEntityManager() {
+        return emf.createEntityManager();
+    }
+    
+    public static Conexao getInstance() throws SQLException {
+        if (conexao == null) {
+            conexao = new Conexao();
+        }
+        return conexao;
+    }
+    
+    public void close() throws SQLException {
+        if (!isClosed()) {
+            conn.close();
+        }
+        conn = null;
+    }
+    
+    public Connection getConn() {
+        return conn;
+    }
+    
+    public Boolean isClosed() throws SQLException {
+        return conn.isClosed();
+    }
+    
+    public static void main(String[] args) {
+        try {
+            Conexao c = Conexao.getInstance();
+            if (!c.isClosed()) {
+                System.out.println("Conexão realizada com sucesso");
+            }
+            c.close();
+            System.out.println("Fechamento realizado com sucesso");
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+    
+}
